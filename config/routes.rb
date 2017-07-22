8be2a8c783e7168d6d142df7a956d1e213c21935
Livio <liviozanardo@gmail.com>
2017-07-22 13:41:56 -0400

    Generated devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'static/welcome'
 
-  resources :users
+  # resources :users
   root 'static#welcome'
   get 'static/welcome'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
