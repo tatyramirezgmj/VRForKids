6769451aec5e04532f3f64c567aa178b279e842c
Livio <liviozanardo@gmail.com>
2017-07-22 19:36:21 -0400

    Preparing for rollback to add database associations among user, games, and game sessions

--- db/schema.rb
+++ db/schema.rb
@@ -10,26 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170722205805) do
+ActiveRecord::Schema.define(version: 20170722155847) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "game_sessions", force: :cascade do |t|
-    t.integer "playtime"
-    t.datetime "date"
-    t.integer "score"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "games", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.string "url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
