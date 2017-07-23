0ca07bb3482429a913e578c158ff7e563b209fea
Livio <liviozanardo@gmail.com>
2017-07-22 20:30:30 -0400

    Updated Database with references using index:true

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170722155847) do
+ActiveRecord::Schema.define(version: 20170723000653) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "game_sessions", force: :cascade do |t|
+    t.integer "playtime"
+    t.datetime "date"
+    t.integer "score"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "games_id"
+    t.index ["games_id"], name: "index_game_sessions_on_games_id"
+  end
+
+  create_table "games", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_games_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,10 +46,6 @@ ActiveRecord::Schema.define(version: 20170722155847) do
     t.datetime "last_sign_in_at"
     t.inet "current_sign_in_ip"
     t.inet "last_sign_in_ip"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
