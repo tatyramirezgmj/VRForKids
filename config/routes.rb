b2da28ac0911d3be40eedd92598149999680122a
Livio <liviozanardo@gmail.com>
2017-07-24 19:56:19 -0400

    Routes adjusted to view addresses - for now.

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,11 @@ Rails.application.routes.draw do
 
   root 'static#welcome'
   get 'static/welcome'
-  get 'game#index', to: 'games/index/'
-  get 'game#show', to: 'games/show/:id'
-  get 'game#play', to: 'games/play/:id'
+  get 'game/index'
+
+  get 'game/:id/show', to: 'game#show', as:'game_show'
+
+  get 'game/:id/show', to: 'game#play', as:'game_play'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
