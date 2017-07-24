6ccb9a9c2b85ee67f5ab29ac613eb81ff954cec4
Livio <liviozanardo@gmail.com>
2017-07-24 19:26:47 -0400

    Game show,index, and play pseudo code built; routes not functional.

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,9 @@ Rails.application.routes.draw do
 
   root 'static#welcome'
   get 'static/welcome'
+  get 'game#index', to: 'games/index/'
+  get 'game#show', to: 'games/show/:id'
+  get 'game#play', to: 'games/play/:id'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
