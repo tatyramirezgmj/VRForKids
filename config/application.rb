ada5d8bcb8b4d0ab79d207896861bf9dd05f467f
kidbougie <kidbougie@users.noreply.github.com>
2017-07-30 15:26:46 -0400

    assets updated

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,7 @@ require_relative 'boot'
 
 require 'rails/all'
 
-config.assets.paths << Rails.root.join("app", "assets", "fonts")
+# config.assets.paths << Rails.root.join("app", "assets", "fonts")
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
