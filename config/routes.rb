89273f53b843474e27b173f9d0e482b713b833fb
tatyramirezgmj <tatiana@trwdesignsolutions.com>
2017-07-30 13:56:30 -0400

    vr game

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,7 @@ Rails.application.routes.draw do
 
   get 'game/:id/play', to: 'game_session#start', as: 'session_start'
   post 'game/:id/play', to: 'game_session#store', as:'session_store'
+
+  get 'game/:id/sounds_game', to: 'game#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
